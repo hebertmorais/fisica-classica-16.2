0974a9301df5d94589a50598d09e8a1b3564c441
Hebert Morais <hebert.morais@embedded.ufcg.edu.br>
2017-03-30 12:59:56 -0300

    Adicionado código de thales e alteracoes nas telas

--- fisica/www/js/controllers.js
+++ fisica/www/js/controllers.js
@@ -1,6 +1,85 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.controller('CalculatorCtrl', function($scope) {
+
+  $scope.data= { magnetica:'', campo:'', carga:'', expForca: '', expCarga: '', velocidade:'', angulo:''};
+
+  $scope.limparCampos = function(){
+    $scope.data.magnetica = undefined;
+    $scope.data.campo = undefined;
+    $scope.data.angulo = undefined;
+    $scope.data.carga = undefined;
+    $scope.data.velocidade = undefined;
+    $scope.data.expCarga = undefined;
+    $scope.data.expForca = undefined;
+
+  }
+
+
+  $scope.calculaForcaMagnetica = function(){
+    console.log("chamou");
+
+    if(!$scope.data.magnetica) {
+      $scope.data.magnetica = parseFloat($scope.data.carga) *
+      parseFloat($scope.data.velocidade) *
+      parseFloat($scope.data.campo) *
+      Math.sin(parseFloat($scope.data.angulo) * (Math.PI / 180));
+
+      console.log('nao magnetica',$scope.data.magnetica, $scope.data.campo,$scope.data.carga, $scope.data.expForca, $scope.data.expCarga, $scope.data.velocidade, $scope.data.angulo );
+
+
+      $scope.data.expForca = $scope.data.expCarga;
+
+      $scope.data.velocidade = undefined;
+      $scope.data.campo = undefined;
+      $scope.data.angulo = undefined;
+      $scope.data.carga = undefined;
+      $scope.data.expCarga = undefined;
+
+    } else if(!$scope.data.carga) {
+      $scope.data.carga = parseFloat($scope.data.magnetica) / (parseFloat($scope.data.velocidade) *
+        parseFloat($scope.data.campo) *
+        Math.sin(parseFloat($scope.data.angulo) * (Math.PI / 180)));
+
+      console.log('nao carga',$scope.data.magnetica );
+
+      $scope.data.expCarga = $scope.data.expForca;
+
+      $scope.data.velocidade = undefined;
+      $scope.data.campo = undefined;
+      $scope.data.angulo = undefined;
+      $scope.data.magnetica = undefined;
+      $scope.data.expForca = undefined;
+
+    } else if(!$scope.data.campo) {
+      $scope.data.campo = parseFloat($scope.data.magnetica) / (parseFloat($scope.data.velocidade) *
+        parseFloat($scope.data.carga) *
+        Math.sin(parseFloat($scope.data.angulo) * (Math.PI / 180)));
+      console.log('nao campo',$scope.data.magnetica );
+
+      $scope.data.velocidade = undefined;
+      $scope.data.magnetica = undefined;
+      $scope.data.angulo = undefined;
+      $scope.data.carga = undefined;
+
+    } else if(!$scope.data.velocidade) {
+      $scope.data.velocidade = parseFloat($scope.data.magnetica) / (parseFloat($scope.data.campo) *
+        parseFloat($scope.data.carga) *
+        Math.sin(parseFloat($scope.data.angulo) * (Math.PI / 180)));
+
+      $scope.data.magnetica = undefined;
+      $scope.data.campo = undefined;
+      $scope.data.angulo = undefined;
+      $scope.data.carga = undefined;
+      console.log('nao velocidade',$scope.data.magnetica );
+
+
+    }
+
+
+
+  }
+})
 
 .controller('ChatsCtrl', function($scope, Chats) {
   // With the new view caching in Ionic, Controllers are only called
@@ -8,21 +87,23 @@ angular.module('starter.controllers', [])
   // To listen for when this page is active (for example, to refresh data),
   // listen for the $ionicView.enter event:
   //
-  //$scope.$on('$ionicView.enter', function(e) {
+  //$scope.data.$on('$ionicView.enter', function(e) {
   //});
 
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
+$scope.chats = Chats.all();
+$scope.remove = function(chat) {
+  Chats.remove(chat);
+};
 })
 
 .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
   $scope.chat = Chats.get($stateParams.chatId);
 })
 
+.controller('DashCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
 .controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
+
 });
